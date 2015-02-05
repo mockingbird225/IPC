60283490bf1fb7955405ff6f8158679b093c4caa
mockingbird225 <anusha136@gmail.com>
2015-02-05 17:33:03 -0600

    updating client and server

--- sockets/client.c
+++ sockets/client.c
@@ -10,46 +10,41 @@
 #include <sys/time.h>
  
 #define MAXRCVLEN 500
-#define PORTNUM 8020
+//#define PORTNUM 8020
 
 struct message {
   struct timeval sent_time;
-  char* data;
+  char* msg;
 };
  
 int main(int argc, char *argv[])
 {
-   //struct message m = (struct message*)malloc(sizeof(struct message));
-   struct message m;
-   char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-   int len, mysocket;
-   struct sockaddr_in dest; 
-   //struct timespec end, start;
-   struct timeval end;
-   mysocket = socket(AF_INET, SOCK_STREAM, 0);
- 
-   memset(&dest, 0, sizeof(dest));                /* zero the struct */
-   dest.sin_family = AF_INET;
-   dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK); /* set destination IP number - localhost, 127.0.0.1*/ 
-   dest.sin_port = htons(PORTNUM);                /* set destination port number */
- 
-   connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
- 
-   len = recv(mysocket, &m, MAXRCVLEN, 0);
+	struct message m;
+	int i, j;
+	int portNum = atoi(argv[1]);
+	char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
+	int mysocket;
+	unsigned long long int minTime = 100, len;
+	struct sockaddr_in dest; 
+	struct timeval end;
+	mysocket = socket(AF_INET, SOCK_STREAM, 0);
 
-   //clock_gettime(CLOCK_MONOTONIC,&end);
- 
-   /* We have to null terminate the received data ourselves */
-   //buffer[len] = '\0';
- 
-   //memcpy(&start,buffer,sizeof(start));
+	memset(&dest, 0, sizeof(dest));                /* zero the struct */
+	dest.sin_family = AF_INET;
+	dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK); /* set destination IP number - localhost, 127.0.0.1*/ 
+	dest.sin_port = htons(portNum);                /* set destination port number */
 
-   printf("Received...\n");
-   gettimeofday(&end, NULL); 
-   unsigned long long int diff = ((end.tv_sec - (m.sent_time).tv_sec)*pow(10,6)) + end.tv_usec - (m.sent_time).tv_usec;
+	connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
 
-   printf("Time diff in microseconds = %lu\n",diff);
-
-   close(mysocket);
-   return EXIT_SUCCESS;
+	for (i = 0; i < 10; i++) {
+		len = recv(mysocket, &m, MAXRCVLEN, 0);
+		gettimeofday(&end, NULL); 
+		unsigned long long int diff = ((end.tv_sec - (m.sent_time).tv_sec)*pow(10,6)) + 
+			end.tv_usec - (m.sent_time).tv_usec;
+		if (diff < minTime) { minTime = diff; } 
+	}
+	/* We have to null terminate the received data ourselves */
+	printf("Time diff in microseconds = %lu\n", minTime);
+	close(mysocket);
+	return EXIT_SUCCESS;
 }
--- sockets/server.c
+++ sockets/server.c
@@ -8,7 +8,7 @@
 #include <sys/socket.h>
 #include <sys/time.h>
  
-#define PORTNUM 8020
+//#define PORTNUM 8020
 
 struct message {
   struct timeval sent_time;
@@ -17,38 +17,36 @@ struct message {
  
 int main(int argc, char *argv[])
 {
-    struct message m;
-    int len=atoi(argv[1]);
-    m.msg = (char *)malloc(atoi(argv[1])*sizeof(char));
-    struct sockaddr_in dest; /* socket info about the machine connecting to us */
-    struct sockaddr_in serv; /* socket info about our server */
-    int mysocket;            /* socket used to listen for incoming connections */
-    socklen_t socksize = sizeof(struct sockaddr_in);
- 
-    memset(&serv, 0, sizeof(serv));           /* zero the struct before filling the fields */
-    serv.sin_family = AF_INET;                /* set the type of connection to TCP/IP */
-    serv.sin_addr.s_addr = htonl(INADDR_ANY); /* set our address to any interface */
-    serv.sin_port = htons(PORTNUM);           /* set the server port number */    
- 
-    mysocket = socket(AF_INET, SOCK_STREAM, 0);
- 
-    /* bind serv information to mysocket */
-    bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
- 
-    printf("Sending...\n");
-   
-    /* start listening, allowing a queue of up to 1 pending connection */
-    listen(mysocket, 1);
-    int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);	
-    //struct timespec start;
-    //clock_gettime(CLOCK_MONOTONIC,&start);
+	struct message m;
+	int i;
+	unsigned long long int len=atoi(argv[1]);
+        int portNum = atoi(argv[2]);
+	m.msg = (char *)malloc(atoi(argv[1])*sizeof(char));
+	struct sockaddr_in dest; /* socket info about the machine connecting to us */
+	struct sockaddr_in serv; /* socket info about our server */
+	int mysocket;            /* socket used to listen for incoming connections */
+	socklen_t socksize = sizeof(struct sockaddr_in);
 
-    //memcpy(msg,&start,sizeof(start));
-    gettimeofday(&m.sent_time, NULL);
-    send(consocket, &m, sizeof(m), 0); 
-    close(consocket);
-    
-    close(mysocket);
-    return EXIT_SUCCESS;
+	memset(&serv, 0, sizeof(serv));           /* zero the struct before filling the fields */
+	serv.sin_family = AF_INET;                /* set the type of connection to TCP/IP */
+	serv.sin_addr.s_addr = htonl(INADDR_ANY); /* set our address to any interface */
+	serv.sin_port = htons(portNum);           /* set the server port number */    
+
+	mysocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	/* bind serv information to mysocket */
+	bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+
+	/* start listening, allowing a queue of up to 1 pending connection */
+	listen(mysocket, 1);
+	int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);	
+        for (i = 0; i < 10; i++) {
+		gettimeofday(&m.sent_time, NULL);
+		send(consocket, &m, len, 0);
+		sleep(1);
+	}
+	close(consocket);
+	close(mysocket);
+	return EXIT_SUCCESS;
 }
 
